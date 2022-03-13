8be558c37de032180211d946f70cfe4458448d55
Uneku <eibkid@gmail.com>
2022-03-13 11:23:24 +0000

    updated the auto date feature

--- saleapp/admin.py
+++ saleapp/admin.py
@@ -4,6 +4,7 @@ from saleapp.models import Extract
 from .models import Extract
 
 
-
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_date', 'updated_date', 'Sales_person', 'Customer_name','customer_country', 'Desired_fruit')
 
 admin.site.register(Extract)
